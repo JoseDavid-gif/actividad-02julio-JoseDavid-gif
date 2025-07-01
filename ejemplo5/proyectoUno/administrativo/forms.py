ea7d64e15d8da95b9ba556063b1db4893ad405fc
JoseDavid-gif <armijosjosedavid@gmail.com>
2025-07-01 10:02:07 -0500

    nuevo

--- ejemplo5/proyectoUno/administrativo/forms.py
+++ ejemplo5/proyectoUno/administrativo/forms.py
@@ -53,6 +53,20 @@ class NumeroTelefonicoForm(ModelForm):
         model = NumeroTelefonico
         fields = ['telefono', 'tipo', 'estudiante']
 
+    def clean_telefono(self):
+        valor = self.cleaned_data['telefono']
+        if len(valor) != 10: 
+            raise forms.ValidationError("Ingrese teléfono con 10 dígitos.")
+        return valor
+
+    def clean_tipo(self):
+        valor = self.cleaned_data['tipo']
+        if valor and valor[0].lower() in 'aeiou':
+            raise forms.ValidationError("El tipo debe comenzar con una consonante.")
+        return valor
+
+
+
 
 class NumeroTelefonicoEstudianteForm(ModelForm):
 
